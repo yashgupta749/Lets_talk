12ffd637d39428c3b4a4d8e9c18e5c977919163c
yashgupta749 <guptayash9808@gmail.com>
2024-07-09 02:37:12 +0530

    deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://lets-talk-40xn.onrender.com", {
         query: {
           userId: authUser._id,
         },
